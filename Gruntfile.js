74606bce9fa2fa352dadf59bc36a48725c3b9b5d
Arnau March <arnau@arnaumarch.com>
2015-05-14 00:28:30 +0200

    updated grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,7 +99,7 @@ module.exports = function(grunt) {
     watch: {
       styles: {
         files: ['../**/*.less'],
-        tasks: ['less','autoprefixer','cssmin'],
+        tasks: ['less','concat','autoprefixer','cssmin'],
         options: {
           nospawn: true
         }
@@ -108,8 +108,11 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('default', [
+    'copy',
     'less',
     'concat',
+    'autoprefixer',
+    'cssmin',
     'watch'
   ]);
 
